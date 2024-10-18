cb6dd88ceb1e11cca697c6cda7f751b1dc6ff33a
amadousadialloh <amadousadialloh@gmail.com>
2024-10-18 08:05:41 -0500

    Exercise 3 completed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const maxOfTwoNumbers = (x, y) => {
   }
 };
 
-// console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
+console.log("Exercise 1 Result:", maxOfTwoNumbers(3, 9));
 
 /*
 Exercise 2: isAdult()
@@ -37,3 +37,26 @@ const isAdult = (age) => {
 };
 
 console.log("Exercise 2 Result:", isAdult(8));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+const isCharAVowel = (char) => {
+  const vowelCharacter = "aeiou";
+  if (vowelCharacter.includes(char.toLowerCase())) {
+    return true;
+  }
+  return false;
+};
+
+console.log("Exercise 3 Result:", isCharAVowel("a"));
