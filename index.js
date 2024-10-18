6e011429e998dfd4ea5394032e048ef317120ea1
amadousadialloh <amadousadialloh@gmail.com>
2024-10-18 09:00:37 -0500

    Exercise 8 completed

--- index.js
+++ index.js
@@ -133,3 +133,31 @@ const maxOfThree = (firsNumber, secondNumber, thirdNumber) => {
 };
 
 console.log("Exercise 6 Result:", maxOfThree(5, 10, 8));
+
+/*
+Exercise 8: convertTemperature()
+
+Write a function named convertTemperature. 
+It takes two arguments: a temperature and a string representing the 
+scale ('C' for Celsius, 'F' for Fahrenheit). 
+Convert the temperature to the other scale.
+
+Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
+Example: convertTemperature(32, 'F') should return 0 (Celsius).
+
+Complete the exercise in the space below:
+*/
+
+const convertTemperature = (temperator, scale) => {
+  scale = scale.toLowerCase();
+  if (scale === "c") {
+    temperator = (temperator * 9) / 5 + 32;
+    return temperator;
+  }
+  if (scale === "f") {
+    temperator = ((temperator - 32) * 5) / 9;
+    return temperator;
+  }
+};
+
+console.log("Exercise 8 Result:", convertTemperature(32, "f"));
